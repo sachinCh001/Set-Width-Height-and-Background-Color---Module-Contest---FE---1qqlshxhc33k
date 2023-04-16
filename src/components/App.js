2e0297ff33e6e5bcef5b20ab31e4209d7a8a1785
sachin <nkrskc@gmail.com>
2023-04-16 09:46:19 +0530

    update

--- src/components/App.js
+++ src/components/App.js
@@ -6,6 +6,30 @@ const App = () => {
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
 
+  const handleWidthChange = (event)=>{
+    setWidth(event.target.value);
+  };
+
+  const handleHeightChange =(event) =>{
+    setHeight(event.target.value);
+  };
+
+  const handleColorChange=(event)=>{
+    setBackgroundColor(event.target.value);
+  };
+
+  const handleCreateBox=()=>{
+    const newBox ={
+      width,
+      height,
+      backgroundColor,
+    };
+    setBoxes([...boxes,newBox]);
+    setWidth("");
+    setHeight("");
+    setBackgroundColor("")
+  };
+
   return (
     <div id="main">
       <h1>Box Creator</h1>
@@ -28,10 +52,25 @@ const App = () => {
         <input
           id="backgroundColor"
           type="text"
+          value={backgroundColor}
+          onChange={handleColorChange}
         />
       </div>
       <button className='create-btn'>Create Box</button>
-      <div id="box-container"></div>
+      <div id="box-container">
+        {
+          boxes.map((box,index)=>(
+            <div key={index}
+            style={{
+              width:`${box.width}px`,
+              height:`${box.height}px`,
+              backgroundColor:box.backgroundColor
+            }}/>
+
+            
+          ))
+        }
+      </div>
     </div>
   );
 };
