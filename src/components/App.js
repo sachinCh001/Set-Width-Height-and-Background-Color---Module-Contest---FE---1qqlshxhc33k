d6b1e762a865710e32c7915d38c76d8dbcabb76b
sachinCh001 <121339822+sachinCh001@users.noreply.github.com>
2023-04-16 09:48:07 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 import '../styles/App.css';
 
 const App = () => {
   const [width, setWidth] = useState("");
   const [height, setHeight] = useState("");
   const [backgroundColor, setBackgroundColor] = useState("");
+  const [boxes, setBoxes] = useState([]);
 
-  const handleWidthChange = (event)=>{
+  const handleWidthChange = (event) => {
     setWidth(event.target.value);
   };
 
-  const handleHeightChange =(event) =>{
+  const handleHeightChange = (event) => {
     setHeight(event.target.value);
   };
 
-  const handleColorChange=(event)=>{
+  const handleColorChange = (event) => {
     setBackgroundColor(event.target.value);
   };
 
-  const handleCreateBox=()=>{
-    const newBox ={
+  const handleCreateBox = () => {
+    const newBox = {
       width,
       height,
       backgroundColor,
     };
-    setBoxes([...boxes,newBox]);
+    setBoxes([...boxes, newBox]);
     setWidth("");
     setHeight("");
-    setBackgroundColor("")
+    setBackgroundColor("");
   };
 
   return (
@@ -38,6 +39,8 @@ const App = () => {
         <input
           id="width"
           type="number"
+          value={width}
+          onChange={handleWidthChange}
         />
       </div>
       <div>
@@ -45,6 +48,8 @@ const App = () => {
         <input
           id="height"
           type="number"
+          value={height}
+          onChange={handleHeightChange}
         />
       </div>
       <div>
@@ -56,24 +61,22 @@ const App = () => {
           onChange={handleColorChange}
         />
       </div>
-      <button className='create-btn'>Create Box</button>
+      <button className='create-btn' onClick={handleCreateBox}>Create Box</button>
       <div id="box-container">
-        {
-          boxes.map((box,index)=>(
-            <div key={index}
+        {boxes.map((box, index) => (
+          <div
+            key={index}
             style={{
-              width:`${box.width}px`,
-              height:`${box.height}px`,
-              backgroundColor:box.backgroundColor
-            }}/>
-
-            
-          ))
-        }
+              width: `${box.width}px`,
+              height: `${box.height}px`,
+              backgroundColor: box.backgroundColor,
+              margin: "10px",
+            }}
+          />
+        ))}
       </div>
     </div>
   );
 };
-
 
 export default App;
